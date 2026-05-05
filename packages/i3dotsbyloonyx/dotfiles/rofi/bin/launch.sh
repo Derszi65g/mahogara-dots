3e658eab249ae8caf6cedf8e4c13be729550404f
Derszi65g <tomalabecker68@gmail.com>
2026-05-05 18:23:01 -0500

    me rendi

--- packages/i3dotsbyloonyx/dotfiles/rofi/bin/launch.sh
+++ packages/i3dotsbyloonyx/dotfiles/rofi/bin/launch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-# launch.sh - Lanzador de aplicaciones para i3dotsbyloonyx
+# launch.sh - Lanzador de aplicaciones (Type-3 Style-3)
 
-# 1. Obtener la ruta del wallpaper actual desde el estado gestionado
+# 1. Obtener la ruta del wallpaper actual
 IMAGE_PATH=$(cat "$HOME/.config/matugen/wallpaper.txt" 2>/dev/null || echo "$HOME/wall/wall.png")
 
-# 2. Obtener el tema configurado desde el entorno
-THEME="${ROFI_THEME:-$HOME/.config/rofi/themes/launcher.rasi}"
+# 2. Definir el tema
+THEME="$HOME/.config/rofi/themes/style-3.rasi"
 
-# 3. Ejecutar Rofi con el override de la imagen de fondo
+# 3. Ejecutar Rofi con la imagen de fondo dinámica en el inputbar
 rofi -show drun -theme "$THEME" -theme-str "inputbar { background-image: url(\"$IMAGE_PATH\", width); }"
